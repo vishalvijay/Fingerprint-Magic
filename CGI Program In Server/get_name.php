ba5b6dbf4af03fa066c14e7f6a6901fa4fc66465
vishalvijay <0vishalvijay0@gmail.com>
2013-02-06 19:59:46 +0530

    Removed unwanted comments

--- CGI Program In Server/get_name.php
+++ CGI Program In Server/get_name.php
@@ -1,57 +1,36 @@
 <?php
 
-/*
- * Following code will get single product details
- * A product is identified by product id (pid)
- */
 
-// array for JSON response
 $response = array();
 
-
-// include db connect class
 require_once __DIR__ . '/db_connect.php';
 
-// connecting to db
 $db = new DB_CONNECT();
 
-// check for post data
 if (isset($_POST["imei"])) {
     $imei = $_POST['imei'];
 
-    // get a product from products table
     $result = mysql_query("SELECT *FROM currentname WHERE imei = $imei");
 
     if (!empty($result)) {
-        // check for empty result
+
         if (mysql_num_rows($result) > 0) {
 
             $result = mysql_fetch_array($result);
-            // success
             $response["success"] = 1;
 			$response["name"]=$result["name"];
 
-            // echoing JSON response
             echo json_encode($response);
         } else {
-            // no product found
             $response["success"] = 0;
-
-            // echo no users JSON
             echo json_encode($response);
         }
     } else {
-        // no product found
         $response["success"] = 0;
-
-        // echo no users JSON
         echo json_encode($response);
     }
 } else {
-    // required field is missing
     $response["success"] = 0;
-
-    // echoing JSON response
     echo json_encode($response);
 }
 ?>
--- CGI Program In Server/make_registration.php
+++ CGI Program In Server/make_registration.php
@@ -1,14 +1,6 @@
 <?php
-
-/*
- * Following code will create a new product row
- * All product details are read from HTTP Post Request
- */
-
-// array for JSON response
 $response = array();
 
-// check for required fields
 if (isset($_POST['imei'])&&isset($_POST['username'])&&isset($_POST['phonenumber'])&&isset($_POST['phonemodel'])&&isset($_POST['appversion'])&&isset($_POST['androidversion'])) {
     
     $imei = $_POST['imei'];
@@ -17,34 +9,21 @@ if (isset($_POST['imei'])&&isset($_POST['username'])&&isset($_POST['phonenumber'
 	$phonemodel= $_POST['phonemodel'];
 	$appversion=  $_POST['appversion'];
 	$androidversion= $_POST['androidversion'];
-    // include db connect class
     require_once __DIR__ . '/db_connect.php';
 
-    // connecting to db
     $db = new DB_CONNECT();
 
-    // mysql inserting a new row
     $result = mysql_query("INSERT INTO userregister(imei,username,phonenumber,phonemodel,appversion,androidversion)VALUES('$imei', '$username','$phonenumber','$phonemodel','$appversion','$androidversion')");
 
-    // check if row inserted or not
     if ($result) {
-        // successfully inserted into database
         $response["success"] = 1;
-
-        // echoing JSON response
         echo json_encode($response);
     } else {
-        // failed to insert row
         $response["success"] = 0;
-        
-        // echoing JSON response
         echo json_encode($response);
     }
 } else {
-    // required field is missing
     $response["success"] = 0;
-
-    // echoing JSON response
     echo json_encode($response);
 }
 ?>
